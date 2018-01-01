2c2ba9b66fe92b0b4e110bd53116a5b714e81c9d
Tin Lam <tin@irrational.io>
2018-01-01 04:17:01 -0600

    Update parameter call
    This patch set updates the ldap* calls in the script and moved
    the admin DN into a variable instead of a static variable.
    
    Change-Id: I4dbf04ec7bd7353bc0072f2bdbe7cf76ef72f52d

--- ldap/memberof.sh
+++ ldap/memberof.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
+set -e
 
 : ${PASSWORD:="password"}
 : ${LDAPHOST:="ldap://localhost"}
+: ${ADMIN:="cn=admin,dc=irrational,dc=io"}
 
-ADMIN="cn=admin,dc=irrational,dc=io"
-
-ldapadd -Q -Y EXTERNAL -H $LDAPHOST -f ldifs/memberof/memberof.ldif
-ldapmodify -Q -Y EXTERNAL -H $LDAPHOST -f ldifs/memberof/refint1.ldif
-ldapadd -Q -Y EXTERNAL -H $LDAPHOST -f ldifs/memberof/refint2.ldif
+ldapadd -Q -Y EXTERNAL -D $ADMIN -H $LDAPHOST -w $PASSWORD -f ldifs/memberof/memberof.ldif
+ldapmodify -Q -Y EXTERNAL -D $ADMIN -H $LDAPHOST -w $PASSWORD -f ldifs/memberof/refint1.ldif
+ldapadd -Q -Y EXTERNAL -D $ADMIN -H $LDAPHOST -w $PASSWORD -f ldifs/memberof/refint2.ldif
--- ldap/setup.sh
+++ ldap/setup.sh
@@ -1,18 +1,17 @@
 #!/usr/bin/env bash
-set -xe
+set -e
 
 : ${PASSWORD:="password"}
 : ${LDAPHOST:="ldap://localhost"}
-
-ADMIN="cn=admin,dc=irrational,dc=io"
+: ${ADMIN:="cn=admin,dc=irrational,dc=io"}
 
 # Add the People group
-ldapadd -x -D $ADMIN -H $LDAPHOST -W -f ldifs/groups/p.ldif
+ldapadd -x -D $ADMIN -H $LDAPHOST -w $PASSWORD -f ldifs/groups/p.ldif
 
 # Add all the users in the users/ folder
 for file in ldifs/users/*.ldif; do
-    ldapadd -x -D $ADMIN -H $LDAPHOST -W -f $file
+    ldapadd -x -D $ADMIN -H $LDAPHOST -w $PASSWORD -f $file
 done
 
 # Add the Groups group
-ldapadd -x -D $ADMIN -H $LDAPHOST -W -f ldifs/groups/g.ldif
+ldapadd -x -D $ADMIN -H $LDAPHOST -w $PASSWORD -f ldifs/groups/g.ldif
